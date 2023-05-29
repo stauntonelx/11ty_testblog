889a8b5479a7fa729bb61117670dfd05b290787a
Stauntonel <stauntonel@gmail.com>
2023-05-29 10:51:37 +0200

    about pagina

---
+++ src/_data/metadata.js
@@ -0,0 +1,11 @@
+module.exports = {
+	title: "Stauntonel blog",
+	url: "https://stauntonel.nl",
+	language: "nl",
+	description: "Aantekeningen van een Linux liefhebber",
+	author: {
+		name: "Stauntonel",
+		email: "stauntonel@gmail.com",
+		url: "https://stauntonel.nl/about-me/"
+	}
+}
